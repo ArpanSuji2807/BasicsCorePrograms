79e8f1ded11843670a28a5d8f9b4bbffab15a000
Praan26 <arpanap102@gmail.com>
2022-04-13 18:48:09 +0530

    [Arpan] Add: Prime Factors added

---
+++ BasicsCorePrograms/BasicsCorePrograms/Factors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsCorePrograms
+{
+    public class Factors
+    {
+        int count = 0;
+
+        public void PrimeFactor()
+        {
+            Console.WriteLine("Enter the number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= number; i++)
+                if (number % i == 0)
+                {
+                    count = 0;
+                    while (number % i == 0)
+                    {
+                        number = number / i;
+                        count++;
+                    }
+                    Console.WriteLine("The prime factors of the number is : " + i);
+                }
+        }
+    }
+}
--- BasicsCorePrograms/BasicsCorePrograms/Program.cs
+++ BasicsCorePrograms/BasicsCorePrograms/Program.cs
@@ -1,7 +1,7 @@
 ﻿using BasicsCorePrograms;
 Console.WriteLine("Welcome to Basic Programs");
 bool end = true;
-Console.WriteLine("1.Flip Coin\n2.Leap Year\n3.Power Of two\n4.Harmonic number");
+Console.WriteLine("1.Flip Coin\n2.Leap Year\n3.Power Of two\n4.Harmonic number\n5.Prime Factors");
 while (end)
 {
     Console.WriteLine("Take an Option to execute");
@@ -25,6 +25,10 @@ while (end)
             number.Harmonic();
             break;
         case 5:
+            Factors n = new Factors();
+            n.PrimeFactor();
+            break;
+        case 6:
             end = false;
             break;
         default: break;
