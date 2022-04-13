ebf51e0a8895b29443239bc6dfd0bbcaa819dee0
Praan26 <arpanap102@gmail.com>
2022-04-13 18:18:14 +0530

    [Arpan] Add: Flip coin added

---
+++ BasicsCorePrograms/BasicsCorePrograms/FlipCoin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsCorePrograms
+{
+    public class FlipCoin
+    {
+        const double HEAD = 0.5;
+        int count_head = 0, count_tail = 0;
+        public void Flip()
+        {
+            Console.WriteLine("Enter the number of times to flip the coin : ");
+            int value = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= value; i++)
+            {
+                Random random = new Random();
+                double a = random.NextDouble();
+                if (a < HEAD)
+                {
+                    count_tail++;
+                }
+                else
+                {
+                    count_head++;
+                }
+            }
+            Console.WriteLine("Head percentage: " + (double)(count_head * 100) / value);
+            Console.WriteLine("Tail percentage: " + (double)(count_tail * 100) / value);
+        }
+    }
+}
--- BasicsCorePrograms/BasicsCorePrograms/Program.cs
+++ BasicsCorePrograms/BasicsCorePrograms/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BasicsCorePrograms;
+Console.WriteLine("Welcome to Basic Programs");
+bool end = true;
+Console.WriteLine("1.Flip Coin");
+while (end)
+{
+    Console.WriteLine("Take an Option to execute");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            FlipCoin flipCoin = new FlipCoin();
+            flipCoin.Flip();
+            break;
+        case 2:
+            end = false;
+            break;
+        default: break;
+    }
+}
